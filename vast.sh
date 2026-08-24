fa4d572ff1790611557db2ec524e3b92d38cd5a9
liz <lzw20160910@sina.com>
2026-08-24 13:12:44 +0800

    ...

--- vast.sh
+++ vast.sh
@@ -19,10 +19,12 @@ bash miniconda.sh -b -p $HOME/miniconda3
 $HOME/miniconda3/bin/conda init bash
 source ~/.bashrc
 
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 conda create -n py312 python=3.12 -y
 conda activate py312
 
 pip3 install torch torchvision
-
+pip install "nvidia-cutlass-dsl[cu13]==4.7.0"
 
 python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
