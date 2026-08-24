1b72ace994392a42dae3fd467754ddf67c8b3d55
liz <lzw20160910@sina.com>
2026-08-24 12:59:18 +0800

    ...

---
+++ vast.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e  # 出错立即停止
+
+
+
+# sudo apt update && sudo apt install -y cmake make build-essential git
+# git clone https://github.com/liziwen-AI/ncutest.git
+# . ncutest/vast.sh
+
+
+sudo apt-get install -y cuda-toolkit-13-0
+sudo apt install micro -y
+export PATH=/usr/local/cuda-13.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-13.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export CUDA_HOME=/usr/local/cuda-13.0
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda3
+$HOME/miniconda3/bin/conda init bash
+source ~/.bashrc
+
+conda create -n py312 python=3.12 -y
+conda activate py312
+
+pip3 install torch torchvision
+
+
+python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
