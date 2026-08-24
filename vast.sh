9e9f6d8a84aec18f68e6c9012bed6d1f289de8ee
liz <lzw20160910@sina.com>
2026-08-24 13:36:07 +0800

    ...

--- vast.sh
+++ vast.sh
@@ -7,9 +7,9 @@ set -e  # 出错立即停止
 # git clone https://github.com/liziwen-AI/ncutest.git
 # . ncutest/vast.sh
 
+sudo apt install micro -y
 
 sudo apt-get install -y cuda-toolkit-13-0
-sudo apt install micro -y
 export PATH=/usr/local/cuda-13.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-13.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export CUDA_HOME=/usr/local/cuda-13.0
@@ -17,10 +17,10 @@ export CUDA_HOME=/usr/local/cuda-13.0
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda3
 $HOME/miniconda3/bin/conda init bash
-# source ~/.bashrc
-
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
 conda create -n py312 python=3.12 -y
 conda activate py312
 
